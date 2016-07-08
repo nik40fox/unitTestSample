9405cb589929a0079916bb29dc93d87b3cdc85b7
nik40fox <nedubrovin@gmail.com>
2016-07-08 09:34:44 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Nikolay on 07.07.2016.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("hello");
+        int y =1;
+        int k;
+        for(k=6; k>=3;k--)
+            y = y + k;
+            System.out.println("y = " + y);
+            System.out.println("k = " + k);
+
+    }
+
+}
